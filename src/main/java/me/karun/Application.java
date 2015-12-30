7a2b60f689593680b4774ee1fdc7d314b09556be
Karun Japhet <karun@japhet.in>
2015-12-30 09:31:02 +0530

    Moving to lombok loggers and making logback more silent

--- src/main/java/me/karun/Application.java
+++ src/main/java/me/karun/Application.java
@@ -1,7 +1,6 @@
 package me.karun;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,22 +9,22 @@ import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-  private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     final ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-    logger.trace("Beans being loaded: ");
+    log.trace("Beans being loaded: ");
 
-    if (logger.isTraceEnabled()) {
+    if (log.isTraceEnabled()) {
       final String[] beanNames = ctx.getBeanDefinitionNames();
       Arrays.sort(beanNames);
       for (final String beanName : beanNames) {
-        logger.trace(beanName);
+        log.trace(beanName);
       }
     }
   }
